b29ba8a7787b730202fe082387fe08f89900ed7c
marcelomedre <marcelo.medre@gmail.com>
2017-01-15 12:47:51 -0200

    Course 3 material

--- Getting and Cleaning Data/Course_3_week_2_quiz.R
+++ Getting and Cleaning Data/Course_3_week_2_quiz.R
@@ -40,8 +40,65 @@ if(length(our_repo) == 0){
 
 library(sqldf)
 library(RMySQL)
+
 url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv"
 download.file(url, destfile = "acs.csv", method = "auto")
-survey <- read.csv("acs.csv", header = TRUE)
-head(survey)
-sqldf("Select pwgtp1 from acs where AGEP < 50")
+
+acs <- read.csv("acs.csv", sep =  ",", header = TRUE)
+
+query1 <- sqldf("Select pwgtp1 from acs where AGEP < 50", drv = "SQLite")
+query2 <- sqldf("select pwgtp1 from acs", drv = "SQLite")
+query3 <- sqldf("select * from acs where AGEP < 50", drv = "SQLite")
+query4 <- sqldf("select * from acs", drv = "SQLite")
+
+
+########################################################################################
+# Question 3
+
+answer <- unique(acs$AGEP)
+
+query1 <- sqldf("select unique AGEP from acs", drv = "SQLite")
+query2 <- sqldf("select distinct AGEP from acs", drv = "SQLite")
+query3 <- sqldf("select AGEP where unique from acs", drv = "SQLite")
+query4 <- sqldf("select distinct pwgtp1 from acs", drv = "SQLite")
+
+identical(answer, query1)
+identical(answer, query2)
+identical(answer, query3)
+identical(answer, query4)
+
+########################################################################################
+# Question 4
+
+library(XML)
+library(httr)
+
+con <- url("http://biostat.jhsph.edu/~jleek/contact.html")
+html <- readLines(con)
+close(con) 
+
+#?nchar()
+answer <- c(nchar(html[10]), nchar(html[20]), nchar(html[30]), nchar(html[100]))
+answer
+
+########################################################################################
+# Question 5
+
+url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fwksst8110.for"
+download.file(url, destfile = "data.for", method = "auto")
+lines <- readLines(url, n=10)
+# defining the widths of the fixed fields
+widths <- c(1, 9, 5, 4, 1, 3, 5, 4, 1, 3, 5, 4, 1, 3, 5, 4, 1, 3)
+
+colNames <- c("filler", "week", "filler", "sstNino12", "filler", 
+              "sstaNino12", "filler", "sstNino3", "filler", "sstaNino3",
+              "filler", "sstNino34", "filler", "sstaNino34", "filler",
+              "sstNino4", "filler", "sstaNino4")
+
+#Read Fixed Width Format Files skiping the 4th column
+
+data <- read.fwf("data.for", widths = widths, header = FALSE, skip = 4, col.names = colNames)
+
+data <- data[, grep("^[^filler]", names(data))]
+answer <- sum(data[,4])
+answer
